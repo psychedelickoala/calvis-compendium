c2a536f35c61f79c192b95d6ac60e97401bd7707
psychedelickoala <77258902+psychedelickoala@users.noreply.github.com>
2022-02-09 18:59:32 +1100

    the searchbar sort of works!!

--- search-script.js
+++ search-script.js
@@ -7,22 +7,62 @@ function myFunction() {
   }
 }
 
+function buildSearchbar() {
+    var x = document.getElementById("searchbar");
+    if (x===null) {
+        $(function(){
+        $("#ghost-menu").load("menu.html");
+        });
+        document.getElementById("searchbar-holder").innerHTML = '<form id="searchbar"><input type="text" placeholder="Search.." name="search"></form><button onclick="searchSite()"><i class="fa fa-search"></i></button>';
+        return true
+    }
+}
+
 function searchSite() {
     var x = document.getElementById("searchbar");
     var text = "";
     var i;
     for (i = 0; i < x.length ;i++) {
+        text += x.elements[i].value;
+    }
+    var terms = text.split(" ");
+    var posts = document.getElementsByClassName("postPreview");
+    var irrelevantPosts = [];
+    var n;
+
+    for (n=0; n < posts.length ; n++) {
+
+        var relevant = false
+        for (i=0; i < terms.length; i++) {
+            if (posts[n].innerHTML.toLowerCase().includes(text.toLowerCase())===true) {
+                relevant=true;
+            }
+        }
+        if (relevant===false) {
+            irrelevantPosts.push(posts[n]);
+        }
+
+    }
+    var its=irrelevantPosts.length;
+    for (n=0; n < its ; n++) {
+        irrelevantPosts[n].remove()
+    }
+
+
+    document.getElementById("search-results").innerHTML = "Your search for \'"+text+ "\' yielded "+(posts.length).toString()+" results!";
+    document.getElementById("searchbar-holder").innerHTML ='<button onclick="buildSearchbar()"><i class="fa fa-search"></i></button>';
+
+
+}
+    /*var text = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
         text += x.elements[i].value + "<br>";
     }
-    $(function(){
-        $("#ghost-menu").load("menu.html");
-        });
 
     var posts = document.getElementsByClassName("postPreview");
-    var results = '';
-    /*for (i=0; i<posts.length; i++) {
+    for (i=0; i<posts.length; i++) {
         if (posts[i].name.includes(text)) {
             posts[i].remove()
-    }*/
-    document.getElementById("search-results").innerHTML = posts[0].name;
-}
+    }
+    document.getElementById("search-results").innerHTML = posts[0].name;*/
